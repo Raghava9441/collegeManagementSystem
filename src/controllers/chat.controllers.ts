8ef015665f0c914c753043248e8ea7ac0766a9a5
Raghavendra Reddy <raghavendra.reddy@htown-tech.com>
2025-03-20 17:39:31 +0530

    Added jwt verification to chat routes initial chat conterollers are ready TODO:1.Testing is pending 2.Reaal time connections need to impliment for real time update of chat

--- src/controllers/chat.controllers.ts
+++ src/controllers/chat.controllers.ts
@@ -1,23 +1,80 @@
+import { Chat } from "@models/chat.modals";
+import { Message } from "@models/message.models";
+import { ApiError } from "@utils/ApiError";
+import { ApiResponse } from "@utils/ApiResponse";
 import { asyncHandler } from "@utils/asyncHandler";
+import { Request, Response } from "express";
 
 const createChat = asyncHandler(async (req: Request, res: Response) => {
 
+    if (!req?.user) {
+        throw new ApiError(404, null, "User not authenticated", undefined, [{ msg: "User not authenticated" }]);
+    }
+
+    const { id: userId, role } = req.user;
+    const { participantId, participantRole } = req.body;
+    //move all request validations to validate function and validate it in route level only not in controllers
+    if (!participantId || !participantRole) {
+        throw new ApiError(404, null, "User not authenticated", undefined, [{ msg: "User not authenticated" }]);
+    }
+
+    let chat = await Chat.findOne({
+        participants: { $all: [{ userId }, { userId: participantId }] },
+    });
+
+    if (!chat) {
+        chat = await Chat.create({
+            participants: [
+                { userId, role },
+                { userId: participantId, role: participantRole },
+            ],
+        });
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, chat, "Chat is created successfully"));
 
 })
 
 const getUserChats = asyncHandler(async (req: Request, res: Response) => {
 
+    if (!req?.user) {
+        throw new ApiError(404, null, "User not authenticated", undefined, [{ msg: "User not authenticated" }]);
+    }
 
+    const chats = await Chat.find({ "participants.userId": req.user.id }).populate("participants.userId", "username role");
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, chats, "user chat fetched successfully"));
 })
 
 const sendMessage = asyncHandler(async (req: Request, res: Response) => {
 
+    if (!req?.user) {
+        throw new ApiError(404, null, "User not authenticated", undefined, [{ msg: "User not authenticated" }]);
+    }
+    const { chatId, message } = req.body;
+    const { id: userId, role } = req.user;
 
+    if (!chatId || !message) {
+        throw new ApiError(404, null, "Chat ID and message are required", undefined, [{ msg: "Chat ID and message are required" }]);
+    }
+
+    const msg = await Message.create({ chatId, senderId: userId, senderRole: role, message });
+    await Chat.findByIdAndUpdate(chatId, { lastMessage: message, lastMessageAt: new Date() });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, msg, "msg send successfull"));
 })
 
 const getMessages = asyncHandler(async (req: Request, res: Response) => {
-
-
+    const messages = await Message.find({ chatId: req.params.chatId }).sort({ createdAt: 1 });
+    return res
+        .status(200)
+        .json(new ApiResponse(200, messages, "msg feteched successfull"));
 })
 
 export {
--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -1,18 +1,36 @@
+import { verifyJWT } from "middlewares/auth.middleware";
 import { createChat, getMessages, getUserChats, sendMessage } from "../controllers/chat.controllers";
 import { Router } from "express";
 
 const router = Router();
 
 // Create a chat (if not already exists)
-router.post("/create", createChat);
+router
+    .post(
+        "/create",
+        verifyJWT,
+        createChat
+    );
 
 // Get all chats for a user
-router.get("/", getUserChats);
+router
+    .get("/",
+        verifyJWT,
+        getUserChats
+    );
 
 // Send a message
-router.post("/send", sendMessage);
+router.post(
+    "/send",
+    verifyJWT,
+    sendMessage
+);
 
 // Get messages from a specific chat
-router.get("/:chatId/messages", getMessages);
+router.get(
+    "/:chatId/messages",
+    verifyJWT,
+    getMessages
+);
 
 export default router;
