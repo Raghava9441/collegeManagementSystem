9d9a8baca33367f2aaa498d78e8ae0eb95bdc23b
Raghavendra Reddy <raghavendra.reddy@htown-tech.com>
2025-04-28 17:27:58 +0530

    used require

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
+const dotenv = require('dotenv');
 import logger from './utils/logger';
 import { app } from './app';
 import connectDB from './db';
